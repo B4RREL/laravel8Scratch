0d04d31f2ff05acfcf8ccb16c259a727249f7652
BarrelIsCoding <oakeminkhant116@gmail.com>
2023-06-12 00:02:51 +0630

    create factory and use it in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,8 +6,8 @@ namespace Database\Seeders;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Catergory;
-use Faker\Provider\Lorem;
-use Faker\Provider\HtmlLorem;
+
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,47 +17,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Catergory::truncate();
-        Post::truncate();
+        // User::truncate();
+        // Catergory::truncate();
+        // Post::truncate();
 
-        $user = \App\Models\User::factory()->create();
+        // $user = \App\Models\User::factory()->create();
+
+        Post::factory()->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $personal = Catergory::create([
-            "name" => "Personal",
-            "slug" => "personal",
-        ]);
+        // $personal = Catergory::create([
+        //     "name" => "Personal",
+        //     "slug" => "personal",
+        // ]);
 
-        $work = Catergory::create([
-            "name" => "Work",
-            "slug" => "work",
-        ]);
+        // $work = Catergory::create([
+        //     "name" => "Work",
+        //     "slug" => "work",
+        // ]);
 
-        $family = Catergory::create([
-            "name" => "Family",
-            "slug" => "family",
-        ]);
+        // $family = Catergory::create([
+        //     "name" => "Family",
+        //     "slug" => "family",
+        // ]);
 
-        Post::create([
-            "title" => "My Post",
-            "user_id" => $user->id,
-            "catergory_id" => $personal->id,
-            "slug" => "my-post",
-            "excerpt" => "lorem ipsum dolor sit amet lore maur convallis dolore magna aliquet",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-        ]);
+        // Post::create([
+        //     "title" => "My Post",
+        //     "user_id" => $user->id,
+        //     "catergory_id" => $personal->id,
+        //     "slug" => "my-post",
+        //     "excerpt" => "lorem ipsum dolor sit amet lore maur convallis dolore magna aliquet",
+        //     "body" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        // ]);
 
-        Post::create([
-            "title" => "My Work Post",
-            "user_id" => $user->id,
-            "catergory_id" => $work->id,
-            "slug" => "my-work-post",
-            "excerpt" => "lorem ipsum dolor sit amet lore maur convallis dolore magna aliquet",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-        ]);
+        // Post::create([
+        //     "title" => "My Work Post",
+        //     "user_id" => $user->id,
+        //     "catergory_id" => $work->id,
+        //     "slug" => "my-work-post",
+        //     "excerpt" => "lorem ipsum dolor sit amet lore maur convallis dolore magna aliquet",
+        //     "body" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        // ]);
     }
 }
